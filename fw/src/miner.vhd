10415e98ecfd6adc272e5086cae31126e821dafa
Ali Lown <ali@lown.me.uk>
2013-01-24 19:04:55 +0000

    Switch to a more parallel design?

--- fw/src/miner.vhd
+++ fw/src/miner.vhd
@@ -24,16 +24,25 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ---- Uncomment the following library declaration if instantiating
 ---- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library UNISIM;
+use UNISIM.VComponents.all;
 
 entity miner is
-  generic ( DEPTH : integer );
+  generic (
+            DEPTH : integer;
+            ID    : integer;
+            TOTAL : integer
+          );
+  --TODO: avoid having these high-speed parallel busses having to be routed
+  --      around the device, by switching to serial. Especially since they force
+  --      timing for a whole 256+32+96 bits in a clock cycle, but are only needed once per ~500+M cycles
+  --      this should also help with the routing congestion
   Port ( clk : in  STD_LOGIC;
-         step : in  STD_LOGIC_VECTOR (5 downto 0);
+         reset : in STD_LOGIC;
          data : in  STD_LOGIC_VECTOR (95 downto 0);
          state : in  STD_LOGIC_VECTOR (255 downto 0);
-         nonce : in  STD_LOGIC_VECTOR (31 downto 0);
+         valid_nonce : out STD_LOGIC_VECTOR(31 downto 0);
+         exhausted_space : out STD_LOGIC;
          hit : out  STD_LOGIC);
 end miner;
 
@@ -59,12 +68,50 @@ architecture Behavioral of miner is
   signal innerhash : std_logic_vector(255 downto 0);
   signal outerhash : std_logic_vector(255 downto 0);
 
+  signal step : std_logic_vector(5 downto 0);
+  signal nonce : std_logic_vector(31 downto 0);
+  signal curnnoce : std_logic_vector(31 downto 0);
+  signal active : std_logic := '0';
 begin
 
   innerdata <= innerprefix & nonce & data;
   outerdata <= outerprefix & innerhash;
   hit <= '1' when outerhash(255 downto 224) = x"00000000" and step = "000000" else '0';
+  valid_nonce <= nonce - 2 * 2 ** DEPTH;
 
+  --Work management
+  process(clk, reset)
+  begin
+    if rising_edge(clk) then
+      if reset = '1' then
+        step <= "000000";
+        nonce <= conv_std_logic_vector(ID, 32);
+        exhausted_space <= '0';
+        active <= '1';
+      else
+        if active = '1' then
+          step <= step + 1;
+          if conv_integer(step) = 2 ** (6 - DEPTH) - 1 then
+            step <= (others => '0');
+            -- todo: change this if E more miners
+            -- prevent overflow
+            if nonce = x"fffffffe" then
+              nonce <= x"ffffffff";
+            else
+              nonce <= nonce + TOTAL;
+            end if;
+          end if;
+          if nonce = x"ffffffff" and step = "000000" then
+            exhausted_space <= '1';
+            active <= '0';
+          end if;
+        end if;
+      end if;
+    end if;
+  end process;
+
+
+  --Work processing
   inner: sha256_pipeline
   generic map ( DEPTH => DEPTH )
   port map (
--- fw/src/top.vhd
+++ fw/src/top.vhd
@@ -37,15 +37,20 @@ end top;
 architecture Behavioral of top is
 
   COMPONENT miner
-    generic ( DEPTH : integer );
-    PORT(
-          clk : IN std_logic;
-          step : IN std_logic_vector(5 downto 0);
-          data : IN std_logic_vector(95 downto 0);
-          state : IN  STD_LOGIC_VECTOR (255 downto 0);
-          nonce : IN std_logic_vector(31 downto 0);
-          hit : OUT std_logic
-        );
+    generic (
+    DEPTH : integer;
+    ID    : integer;
+    TOTAL : integer
+  );
+  PORT(
+        clk : IN std_logic;
+        reset : IN std_logic;
+        data : IN std_logic_vector(95 downto 0);
+        state : IN  STD_LOGIC_VECTOR (255 downto 0);
+        valid_nonce : OUT std_logic_vector(31 downto 0);
+        exhausted_space : out STD_LOGIC;
+        hit : OUT std_logic
+      );
   END COMPONENT;
 
   COMPONENT uart
@@ -77,24 +82,23 @@ architecture Behavioral of top is
   signal clk_dcmout : std_logic;
   signal data : std_logic_vector(95 downto 0);
   signal state : std_logic_vector(255 downto 0);
-  signal nonce : std_logic_vector(31 downto 0);
-  signal currnonce : std_logic_vector(31 downto 0);
   signal load : std_logic_vector(343 downto 0);
   signal loadctr : std_logic_vector(5 downto 0);
   signal loading : std_logic := '0';
-  signal hit : std_logic;
   signal txdata : std_logic_vector(48 downto 0);
   signal txwidth : std_logic_vector(5 downto 0);
   signal txstrobe : std_logic;
   signal rxdata : std_logic_vector(7 downto 0);
   signal rxstrobe : std_logic;
-  signal step : std_logic_vector(5 downto 0) := "000000";
   signal locked : std_logic;
 
+  signal miner_reset : std_logic;
+  signal exhausted_space : std_logic_vector(1 downto 0);
+  signal hit             : std_logic_vector(1 downto 0);
+  type nonces is array (1 downto 0) of std_logic_vector(31 downto 0);
+  signal valid_nonce    : nonces;
+
 begin
-
-
-  currnonce <= nonce - 2 * 2 ** DEPTH;
 
   inst_dcm : bitdcm
   port map (
@@ -106,15 +110,38 @@ begin
              LOCKED => locked
            );
 
+  --todo: turn the miners into a generate statement
   miner0: miner
-  generic map ( DEPTH => DEPTH )
+  generic map (
+                DEPTH => DEPTH,
+                ID    => 0,
+                TOTAL => 2
+              )
   port map (
              clk => clk,
-             step => step,
+             reset => miner_reset,
              data => data,
              state => state,
-             nonce => nonce,
-             hit => hit
+             exhausted_space => exhausted_space(0),
+             valid_nonce => valid_nonce(0),
+             hit => hit(0)
+           );
+
+  --TODO: run this one in parallel testing the other half of the space (if it fits)
+  miner1: miner
+  generic map (
+                DEPTH => DEPTH,
+                ID    => 1,
+                TOTAL => 2
+              )
+  port map (
+             clk => clk,
+             reset => miner_reset,
+             data => data,
+             state => state,
+             exhausted_space => exhausted_space(1),
+             valid_nonce => valid_nonce(1),
+             hit => hit(1)
            );
 
   serial: uart
@@ -135,57 +162,58 @@ begin
   process(clk)
   begin
     if rising_edge(clk) then
-      step <= step + 1;
-      if conv_integer(step) = 2 ** (6 - DEPTH) - 1 then
-        step <= "000000";
-        nonce <= nonce + 1;
+      txdata <= "0000000000000000000000000000000000000000000000000";
+      txwidth <= "000000";
+      txstrobe <= '0';
+      if rxstrobe = '1' then
+        if loading = '1' then
+          if loadctr = "101011" then
+            leds(2 downto 0) <= "100";
+            state <= load(343 downto 88);
+            data <= load(87 downto 0) & rxdata;
+            miner_reset <= '0';
+            txdata <= "1111111111111111111111111111111111111111000000010";
+            txwidth <= "001010";
+            txstrobe <= '1';
+            loading <= '0';
+          else
+            leds(2 downto 0) <= "101";
+            load(343 downto 8) <= load(335 downto 0);
+            load(7 downto 0) <= rxdata;
+            loadctr <= loadctr + 1;
+            miner_reset <= '1';
+          end if;
+        else
+          if rxdata = "00000000" then
+            leds(2 downto 0) <= "110";
+            txdata <= "1111111111111111111111111111111111111111000000000";
+            txwidth <= "001010";
+            txstrobe <= '1';
+          elsif rxdata = "00000001" then
+            leds(2 downto 0) <= "111";
+            loadctr <= "000000";
+            loading <= '1';
+          end if;
+        end if;
+      elsif hit(0) = '1' then
+        leds(2 downto 0) <= "010";
+        txdata <= valid_nonce(0)(7 downto 0) & "01" & valid_nonce(0)(15 downto 8) & "01" & valid_nonce(0)(23 downto 16) & "01" & valid_nonce(0)(31 downto 24) & "01000000100";
+        txwidth <= "110010";
+        txstrobe <= '1';
+      elsif hit(1) = '1' then
+        leds(2 downto 0) <= "010";
+        txdata <= valid_nonce(1)(7 downto 0) & "01" & valid_nonce(1)(15 downto 8) & "01" & valid_nonce(1)(23 downto 16) & "01" & valid_nonce(1)(31 downto 24) & "01000000100";
+        txwidth <= "110010";
+        txstrobe <= '1';
+      elsif exhausted_space(0) = '1' and exhausted_space(1) = '1' then
+        leds(2 downto 0) <= "011";
+        txdata <= "1111111111111111111111111111111111111111000000110";
+        txwidth <= "110010";
+        txstrobe <= '1';
       end if;
-      txdata <= "-------------------------------------------------";
-                txwidth <= "------";
-                           txstrobe <= '0';
-                           if rxstrobe = '1' then
-                             if loading = '1' then
-                               if loadctr = "101011" then
-                                 leds(2 downto 0) <= "100";
-                                 state <= load(343 downto 88);
-                                 data <= load(87 downto 0) & rxdata;
-                                 nonce <= x"00000000";
-                                 txdata <= "1111111111111111111111111111111111111111000000010";
-                                 txwidth <= "001010";
-                                 txstrobe <= '1';
-                                 loading <= '0';
-                               else
-                                 leds(2 downto 0) <= "101";
-                                 load(343 downto 8) <= load(335 downto 0);
-                                 load(7 downto 0) <= rxdata;
-                                 loadctr <= loadctr + 1;
-                               end if;
-                             else
-                               if rxdata = "00000000" then
-                                 leds(2 downto 0) <= "110";
-                                 txdata <= "1111111111111111111111111111111111111111000000000";
-                                 txwidth <= "001010";
-                                 txstrobe <= '1';
-                               elsif rxdata = "00000001" then
-                                 leds(2 downto 0) <= "111";
-                                 loadctr <= "000000";
-                                 loading <= '1';
-                               end if;
-                             end if;
-                           elsif hit = '1' then
-                             leds(2 downto 0) <= "010";
-                             txdata <= currnonce(7 downto 0) & "01" & currnonce(15 downto 8) & "01" & currnonce(23 downto 16) & "01" & currnonce(31 downto 24) & "01000000100";
-                             txwidth <= "110010";
-                             txstrobe <= '1';
-                           elsif nonce = x"ffffffff" and step = "000000" then
-                             leds(2 downto 0) <= "011";
-                             txdata <= "1111111111111111111111111111111111111111000000110";
-                             txwidth <= "110010";
-                             txstrobe <= '1';
-                           end if;
-              end if;
-    end process;
+    end if;
+  end process;
 
 
-  end Behavioral;
+end Behavioral;
 
